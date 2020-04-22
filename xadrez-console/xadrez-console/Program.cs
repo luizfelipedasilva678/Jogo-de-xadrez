acddb4caad1e994796343693ba360bb108f62dca
Luiz Felipe <luizfelipedasilva678@gmail.com>
2020-04-22 20:50:13 -0300

    Implementando a lógica de xequemate

--- xadrez-console/xadrez-console/Program.cs
+++ xadrez-console/xadrez-console/Program.cs
@@ -42,8 +42,8 @@ namespace xadrez_console
                         Console.ReadLine();
                     }
                 }
-
-                Tela.imprimirTabuleiro(partida.tab);
+                Console.Clear();
+                Tela.imprimirPartida(partida);
             }catch(TabuleiroException e)
             {
                 Console.WriteLine(e.Message);
